f81e08687348b3ae6b81f11f4b50b131bf217239
nazmu957 <nazmus15-1999@diu.edu.bd>
2023-11-01 22:32:28 +0600

    primary project setup

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import ImageGallery from "./components/ImageGallery";
+
+
+function App() {
+  return (
+    <>
+      <div>
+        <ImageGallery />
+      </div>
+    </>
+  );
+}
+
+export default App;
