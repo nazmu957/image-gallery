029138a4de38d4c5bfbd1726a7da0cebd6a91666
nazmu957 <nazmus15-1999@diu.edu.bd>
2023-11-02 09:59:50 +0600

    implement drag and drop feature

--- src/components/ImageGallery.jsx
+++ src/components/ImageGallery.jsx
@@ -1,91 +1,83 @@
-import  { useState } from "react";
+import { useState } from "react";
 
 const ImageGallery = () => {
-  const [uploadedImage, setUploadedImage] = useState(null);
   const [images, setImages] = useState([
     "https://i.ibb.co/vc6pqZ3/logo.jpg",
+    "https://i.ibb.co/4pJN4wJ/pc-4.jpg",
     "https://i.ibb.co/vc6pqZ3/logo.jpg",
     "https://i.ibb.co/vc6pqZ3/logo.jpg",
     "https://i.ibb.co/vc6pqZ3/logo.jpg",
     "https://i.ibb.co/vc6pqZ3/logo.jpg",
     "https://i.ibb.co/vc6pqZ3/logo.jpg",
     "https://i.ibb.co/vc6pqZ3/logo.jpg",
+    "https://i.ibb.co/YRr2Bbm/pic6.jpg",
     "https://i.ibb.co/vc6pqZ3/logo.jpg",
-    // ... rest of your image URLs
+    "https://i.ibb.co/vc6pqZ3/logo.jpg",
+    "https://i.ibb.co/vc6pqZ3/logo.jpg",
+    // ... rest of your initial image URLs
   ]);
 
-  const handleImageUpload = (event) => {
-    const file = event.target.files[0];
-    if (file) {
-      const reader = new FileReader();
-      reader.onloadend = () => {
-        setUploadedImage(reader.result);
-        setImages([...images, reader.result]); // Update the images state with the uploaded image
-      };
-      reader.readAsDataURL(file);
-    }
+  const handleDragStart = (e, index) => {
+    e.dataTransfer.setData("index", index);
+  };
+
+  const handleDragOver = (e) => {
+    e.preventDefault();
+  };
+
+  const handleDrop = (e, targetIndex) => {
+    const sourceIndex = e.dataTransfer.getData("index");
+    const updatedImages = [...images];
+    const [draggedImage] = updatedImages.splice(sourceIndex, 1);
+    updatedImages.splice(targetIndex, 0, draggedImage);
+    setImages(updatedImages);
   };
 
   return (
-    <div className="flex flex-col sm:flex-row sm:flex-wrap">
-      {/* Large Image Card */}
-      <div className="w-full sm:w-1/2 p-2">
-        <div className="bg-white rounded-lg overflow-hidden shadow-lg">
-          <img src={images[0]} alt="Large" className="w-full h-auto" />
-        </div>
-      </div>
-      {/* Small Image Cards (first row) */}
-      <div className="w-full sm:w-1/2 sm:flex flex-wrap">
-        {images.slice(1, 7).map((image, index) => (
-          <div key={index} className="w-1/3 p-2">
-            <div className="bg-white rounded-lg overflow-hidden shadow-lg">
-              <img
-                src={image}
-                alt={`Small ${index}`}
-                className="w-full h-auto"
-              />
-            </div>
-          </div>
-        ))}
-      </div>
-      {/* Small Image Cards (second row) */}
-      <div className="w-full sm:flex flex-wrap">
-        {images.slice(7).map((image, index) => (
-          <div key={index} className="w-1/5 p-2">
-            <div className="bg-white rounded-lg overflow-hidden shadow-lg">
-              <img
-                src={image}
-                alt={`Small ${index}`}
-                className="w-full h-auto"
-              />
-            </div>
-          </div>
-        ))}
-        {/* New Image Upload Card */}
-        <div className="w-1/5 p-2">
+    <div className=" ">
+      <div className="grid grid-cols-1 sm:grid-cols-2 w-full">
+        {/* Large Image */}
+        <div className="p-2">
           <div className="bg-white rounded-lg overflow-hidden shadow-lg">
-            {uploadedImage ? (
-              <img
-                src={uploadedImage}
-                alt="Uploaded"
-                className="w-full h-auto"
-              />
-            ) : (
-              <label
-                htmlFor="imageUpload"
-                className="cursor-pointer block w-full h-40 border-dashed border-2 border-gray-300 flex justify-center items-center"
-              >
-                Click to upload image
-                <input
-                  type="file"
-                  id="imageUpload"
-                  className="hidden"
-                  onChange={handleImageUpload}
-                />
-              </label>
-            )}
+            <img src={images[0]} alt="Large" className="w-full h-auto" />
           </div>
         </div>
+
+        {/* Small Image Cards (right side) */}
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 p-2">
+          {images.slice(1, 7).map((image, index) => (
+            <div
+              key={index}
+              className="bg-white rounded-lg overflow-hidden shadow-lg"
+              draggable
+              onDragStart={(e) => handleDragStart(e, index + 1)}
+              onDragOver={handleDragOver}
+              onDrop={(e) => handleDrop(e, index + 1)}
+            >
+              <img
+                src={image}
+                alt={`Small ${index}`}
+                className="w-full h-auto"
+              />
+            </div>
+          ))}
+        </div>
+      </div>
+
+      {/* Small Image Cards (second row) */}
+      <div className="grid grid-cols-5 gap-4">
+        {images.slice(7).map((image, index) => (
+          <div
+            key={index + 7}
+            className="bg-white rounded-lg overflow-hidden shadow-lg"
+            draggable
+            onDragStart={(e) => handleDragStart(e, index + 7)}
+            onDragOver={handleDragOver}
+            onDrop={(e) => handleDrop(e, index + 7)}
+          >
+            <img src={image} alt={`Small ${index}`} className="w-full h-auto" />
+          </div>
+        ))}
       </div>
     </div>
   );
